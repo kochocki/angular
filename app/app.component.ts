01d94d84bd63b2eccc3684864600a203e1808092
Krzysztof Ochocki <krzysztof.ochocki1@gmail.com>
2016-06-01 23:31:37 +0200

    added app structure

---
+++ app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+import { DashboardComponent } from './dashboard.component';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
+import { HeroDetailComponent } from './hero-detail.component';
+
+@Component({
+  selector: 'my-app',
+  template: `
+  <h1>{{title}}</h1>
+  <nav>
+    <a [routerLink]="['Dashboard']">Dashboard</a>
+    <a [routerLink]="['Heroes']">Heroes</a>
+  </nav>
+  <router-outlet></router-outlet>
+`,
+  directives: [ROUTER_DIRECTIVES],
+  providers: [ROUTER_PROVIDERS,HeroService]
+})
+
+@RouteConfig([
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true
+  },
+  {
+    path: '/heroes',
+    name: 'Heroes',
+    component: HeroesComponent
+  },
+  {
+    path: '/detail/:id',
+    name: 'HeroDetailComponent',
+    component: HeroDetailComponent
+  }
+])
+
+export class AppComponent {
+  title = 'Tour of Heroes'
+
+}
